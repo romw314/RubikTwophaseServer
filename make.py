2a3d6f058e9c08f7d84acc8aac98cd4c94993752
romw314 <106016361+romw314@users.noreply.github.com>
2023-10-05 15:22:22 +0200

    Fixed make.py

--- make.py
+++ make.py
@@ -1,3 +1,10 @@
+import os;
+
+try:
+	os.mkdir("twophase");
+except FileExistsError:
+	print("the twophase directory already exists: skipped creting it");
+
 import twophase.pruning;
 import twophase.symmetries;
 import twophase.coord;
